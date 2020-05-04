e26fd3f41bbb09a0ac0db88b79e5c5a71bb49cfe
nitin jain <nitin.e@gmail.com>
2020-05-04 23:59:37 +0530

    cleanup

--- app/src/main/java/id/ac/nitin/apicovid/MainActivity.java
+++ app/src/main/java/id/ac/nitin/apicovid/MainActivity.java
@@ -78,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                //String value = dataSnapshot.getValue(String.class);
                 DecimalFormat thousand = new DecimalFormat("#,###");
                 TextView tvGlobalConfirmed = findViewById(R.id.tvGlobalConfirmed);
                 TextView tvGlobalNewConfirmed = findViewById(R.id.tvGlobalNewConfirmed);
